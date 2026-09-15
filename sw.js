e6fd8f747f5f097df48da5c875ce6a5e4d3d3a78
kcinick00 <ricodelico19@gmail.com>
2026-09-14 21:42:19 -0400

    Update cache name and modify cached files
    Updated cache name to 'sandwich-app-v9' and added 'voice.js' to the cache list. Removed several image layers from the cache.

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE_NAME = "sandwich-app-v8";
+const CACHE_NAME = "sandwich-app-v9";
 const FILES_TO_CACHE = [
   "index.html",
   "app.js",
+  "voice.js",
   "ingredients.js",
   "manifest.json",
   "icon-192.png",
   "icon-512.png",
-  // Imágenes de la selección (menú)
   "images/aguacate.jpg",
   "images/baguette.jpg",
   "images/bbq.jpg",
@@ -26,32 +26,7 @@ const FILES_TO_CACHE = [
   "images/res.jpg",
   "images/salami.jpg",
   "images/telera.jpg",
-  "images/tocino.jpg",
-  // Imágenes de las capas del sándwich (si las tienes)
-  "images/layers/lechuga.png",
-  "images/layers/jitomate.png",
-  "images/layers/cebolla.png",
-  "images/layers/aguacate.png",
-  "images/layers/jamon.png",
-  "images/layers/pavo.png",
-  "images/layers/salami.png",
-  "images/layers/tocino.png",
-  "images/layers/pollo.png",
-  "images/layers/res.png",
-  "images/layers/queso-panela.png",
-  "images/layers/huevo.png",
-  "images/layers/mayonesa.png",
-  "images/layers/mostaza.png",
-  "images/layers/chipotle.png",
-  "images/layers/bbq.png",
-  "images/layers/pan-blanco-top.png",
-  "images/layers/pan-blanco-bottom.png",
-  "images/layers/pan-integral-top.png",
-  "images/layers/pan-integral-bottom.png",
-  "images/layers/telera-top.png",
-  "images/layers/telera-bottom.png",
-  "images/layers/baguette-top.png",
-  "images/layers/baguette-bottom.png"
+  "images/tocino.jpg"
 ];
 
 self.addEventListener("install", (event) => {
